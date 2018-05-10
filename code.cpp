fb4c4543d545d43c96a4032c7c2eecf05bea27ee
Oleg <31900171+Olegatorr@users.noreply.github.com>
2018-05-10 14:29:26 +0300

    first commit, lets see

---
+++ code.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <conio.h>
+#define REE Peripherals::Instance()->
+
+class Peripherals
+{
+protected:
+	static Peripherals* _self;
+	Peripherals() {}
+	virtual ~Peripherals() {}
+
+public:
+	static Peripherals* Instance()
+	{
+		if (!_self)
+		{
+			_self = new Peripherals();
+		}
+		return _self;
+	}
+	int asd = 100;
+	void interface() {
+
+		printf("sdasdasd");
+
+
+	}
+
+	void show() {
+
+
+
+	}
+
+	void add() {
+
+
+
+	}
+
+	static bool DeleteInstance()
+	{
+		if (_self)
+		{
+			delete _self;
+			_self = 0;
+			return true;
+		}
+		return false;
+	}
+};
+
+Peripherals* Peripherals::_self = 0;
+
+int main()
+{
+
+	Peripherals::Instance()->interface();
+	REE asd+=100;
+	printf("%d",REE asd);
+
+}
